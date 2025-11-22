7c03f846a9db77dba99d51d41f20e8485fe427c4
p1670268-maker <p1670268@gmail.com>
2025-11-22 10:05:15 +0530

    Add files via upload

---
+++ struct_calls.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+struct score
+{
+    float marks;
+    int rank;
+};
+void edit (struct score s1)
+{
+    printf("before cvhange in fn : %f\n", s1.marks);
+    s1.marks = 34.6;
+    printf("after change in fn :%f\n",s1.marks);
+}
+void main() {
+    struct score s1 = {87.9,5};
+    edit(s1);
+    printf("after change in main %f\n",s1.marks);
+}
